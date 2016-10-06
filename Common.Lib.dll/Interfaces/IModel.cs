585aed919583e19c894a87a7f2f86113d08c0d59
Kevin Heugen <kevinheugen1996@gmail.com>
2016-10-06 23:26:42 +0200

    Stap 1 en 2 van MVP tutorial

---
+++ Common.Lib.dll/Interfaces/IModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Lib.Interfaces
+{
+    interface IModel : IBaseInterface
+    {
+        DataSet GetJobData (string filename);
+    }
+}
--- MEP/Form1.cs
+++ MEP/Form1.cs
@@ -8,10 +8,14 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace MEP
 {
     public partial class Form1 : Form
     {
+
+
+
         public Form1()
         {
             InitializeComponent();
