c79012e951abd35b2bb24cd52f8248122cd0c961
Kevin Heugen <kevinheugen1996@gmail.com>
2016-10-07 18:44:57 +0200

    Stap 3 van MVP tutorial

---
+++ Common.Lib.dll/Models/Model.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Lib.Interfaces;
+using System.Data.OleDb;
+
+namespace Common.Lib.Models
+{
+    class Model : IModel
+    {
+        public DataSet GetJobData(string filename)
+        {
+            string connectionString =”Provider = Microsoft.Jet.OLEDB.4.0; Data Source = c:\csvfiles\; Extended Properties =’text; HDR = Yes'”;
+
+            string commandText = string.Format(“Select * from { 0}”, filename);
+
+            OleDbConnection conn = new OleDbConnection(connectionString);
+
+            OleDbCommand cmd = new OleDbCommand(commandText, conn);
+
+
+
+            conn.Open();
+
+            OleDbDataAdapter da = new OleDbDataAdapter(commandText, conn);
+
+            DataSet JobData = new DataSet();
+
+            da.Fill(JobData);
+
+            return JobData;
+        }
+    }
+}
