25a4be1ff26ef1e8bdaf721ecfdd368962834436
Kevin Heugen <kevinheugen1996@gmail.com>
2016-10-10 23:09:06 +0200

    Fun Chap1.1 gedaan

---
+++ FunChap1/Chap1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Fun
+{
+    static class Chap1
+    {
+        static public void Excersice1()
+        {
+            int var1 = 52130;
+            Console.WriteLine(var1);
+            short var2 = -115;
+            Console.WriteLine(var2);
+            int var3 = 4825932;
+            Console.WriteLine(var3);
+            byte var4 = 97;
+            Console.WriteLine(var4);
+            short var5 = -10000;
+            Console.WriteLine(var5);
+            ushort var6 = 20000;
+            Console.WriteLine(var6);
+            byte var7 = 224;
+            Console.WriteLine(var7);
+            int var8 = 970700000;
+            Console.WriteLine(var8);
+            byte var9 = 112;
+            Console.WriteLine(var9);
+            sbyte var10 = -44;
+            Console.WriteLine(var10);
+            int var11 = -1000000;
+            Console.WriteLine(var11);
+            short var12 = 1990;
+            Console.WriteLine(var12);
+            long var13 = 123456789123456789;
+            Console.WriteLine(var13);
+
+            Console.ReadLine();
+        }
+
+        static public void Exercise2()
+        {
+
+        }
+    }
+}
---
+++ FunChap1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Chap1.Excersice1();
+        }
+    }
+}
